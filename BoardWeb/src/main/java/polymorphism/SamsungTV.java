bbefca509e9e26f74311f5ef3de2da35cf05fc62
seminocoe <qhrms9595@naver.com>
2022-09-08 18:13:23 +0900

    20220908
    20220908

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -1,9 +1,46 @@
 package polymorphism;
 
 public class SamsungTV implements TV{
+	
+	private Speaker speaker;
+	private int price;
 
+	
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===>setSpeaker()호출");
+		this.speaker = speaker;
+	}
+
+	public void setPrice(int price) {
+		System.out.println("===>setPrice()호출");
+		this.price = price;
+	}
+
+	
+	public void initMethod() {
+		System.out.println("객체 초기화 작업을 처리함......");
+	}
+	
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전에 처리할 로직을......");
+	}
+	
+	public SamsungTV() {
+		System.out.println("====>삼성티비(1) 객체 생성...");
+	}
+	public SamsungTV(Speaker speaker) {
+		System.out.println("====>삼성티비(2) 객체 생성...");
+		this.speaker = speaker;
+	}
+	public SamsungTV(Speaker speaker, int price) {
+		System.out.println("====>삼성티비(3) 객체 생성...");
+		this.speaker = speaker;
+		this.price = price;
+	}
+	
 	public void powerOn(){
-		System.out.println("삼성티비 ------ 전원 켠다.");
+//		System.out.println("삼성티비 ------ 전원 켠다.");
+		System.out.println("삼성티비 ------ 전원 켠다.(가격:"+price+")");
 	}
 	
 	public void powerOff(){
@@ -11,11 +48,15 @@ public class SamsungTV implements TV{
 	}
 	
 	public void volumeUp(){
-		System.out.println("삼성티비 ------ 소리 올린다.");
+//		System.out.println("삼성티비 ------ 소리 올린다.");
+//		speaker = new SonySpeaker();
+		speaker.volumeUp();
 	}
 	
 	public void volumeDown(){
-		System.out.println("삼성티비 ------ 소리 내린다.");
+//		System.out.println("삼성티비 ------ 소리 내린다.");
+//		speaker = new SonySpeaker();
+		speaker.volumeDown();
 	}
 	
 }
